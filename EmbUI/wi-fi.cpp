3bde800b80d436dd9fb16634cd708e03fc517ece
Emil Muratov <gpm@hotplug.ru>
2020-11-25 20:29:02 +0300

    WiFi status fix  - fixed updating WiFi station variable (ESP32)  - Interface frame size increased for ESP32
    Signed-off-by: Emil Muratov <gpm@hotplug.ru>

--- EmbUI/wi-fi.cpp
+++ EmbUI/wi-fi.cpp
@@ -100,6 +100,7 @@ void EmbUI::WiFiEvent(WiFiEvent_t event, WiFiEventInfo_t info)
         }
 
         embuischedw.detach();
+        sysData.wifi_sta = true;
         setup_mDns();
         if(_cb_STAGotIP)
             _cb_STAGotIP();        // execule callback
@@ -113,6 +114,8 @@ void EmbUI::WiFiEvent(WiFiEvent_t event, WiFiEventInfo_t info)
                                                         LOG(println, F("UI WiFi: Switch to AP-Station mode"));
                                                         embuischedw.detach();} );
         }
+
+        sysData.wifi_sta = false;
         if(_cb_STADisconnected)
             _cb_STADisconnected();        // execule callback
         break;
